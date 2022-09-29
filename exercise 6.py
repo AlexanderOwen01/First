40e86f2e73684e607bcb59aa4c54f7389d357b6b
Split Bill Calculator_Owen <alexander.owen62@gmail.com>
2022-09-29 18:44:00 +0700

    save

---
+++ exercise 6.py
@@ -0,0 +1,6 @@
+burger = float(input("enter burger selling price:$"))
+soda = float(input("enter soda selling price:$"))
+combo = float(input("enter combo meal selling price:$"))
+
+profit= (burger+soda)-combo
+print(profit)
